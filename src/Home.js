57625eb9bc7e816e463ffab13118b27e782326b7
Huseyin Eskan <huseyineskan01@gmail.com>
2024-09-18 23:21:13 +0200

    props and reusing components

--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,15 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
-        {title: "My new website", body: "lorem ipsum...", author: 'Huseyin', id:1},
-        {title: "Welcome Party!", body: "lorem ipsum...", author: 'Gözde', id:2},
-        {title: "Web dev top tips!", body: "lorem ipsum...", author: 'Mirza', id:3}
+        {title: "My new website", body: "This is huseyin's text", author: 'Huseyin', id:1},
+        {title: "Welcome Party!", body: "This is gozde's text", author: 'Gözde', id:2},
+        {title: "Web dev top tips!", body: "This is mirza's text", author: 'Mirza', id:3}
     ]);
 
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === "Huseyin")} title="Huseyin's Blog!"/>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>The Blog</h1>
+            <h1>React Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
