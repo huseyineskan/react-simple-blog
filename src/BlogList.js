44dbaa506f3a6c7223c1c9ae97cd34de13072341
Huseyin Eskan <huseyineskan01@gmail.com>
2024-09-20 23:56:19 +0200

    added delete blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ( {title, blogs} ) => {
+const BlogList = ( {title, blogs, handleDelete} ) => { 
     return ( 
         <div className="blog-list">
             <h2>{ title }</h2>
@@ -6,6 +6,7 @@ const BlogList = ( {title, blogs} ) => {
                 <div className='blog-preview' key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
+                    <button className="blog-delete-btn" onClick={ () => handleDelete(blog.id) }>Delete blog</button>
                 </div>
             ))}
         </div>
