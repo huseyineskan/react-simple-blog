8a9461140724ad578f4bab1cc09adf6b7c2da8ca
Huseyin Eskan <huseyineskan01@gmail.com>
2024-09-22 00:08:31 +0200

    useEffect independencies

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,9 @@ const Home = () => {
         {title: "Web dev top tips!", body: "This is mirza's text", author: 'Mirza', id:3}
     ]);
 
+    const [name, setName] = useState('Huseyin');
+    const [lname, setLname] = useState('Eskan');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -15,12 +18,15 @@ const Home = () => {
 
     useEffect(() => {
         console.log("useEffect ran")
-        console.log(blogs);
-    });
+        console.log(name, lname)
+    }, [name, lname]);
 
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('Gözde')}>Change name</button>
+            <button onClick={() => setLname('Karabekiroğlu')}>Change lname</button>
+            <p>{ name } { lname }</p>
         </div>
     );
 }
