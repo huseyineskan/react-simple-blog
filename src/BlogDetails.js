419a717d5c2e997cdb4391fa0c1e90c0e3f6f259
Huseyin Eskan <huseyineskan01@gmail.com>
2024-10-04 23:17:48 +0200

    added breadcrumbs on the blog details page

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,11 @@
 import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const BlogDetails = () => {
     const { id } = useParams(); 
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
-    const history = useHistory();
+    const history = useHistory();    
 
     const handleClick = () => {
         fetch('http://localhost:8000/blogs/' + blog.id, {
@@ -20,6 +21,11 @@ const BlogDetails = () => {
             { error && <div>{ error }</div> }
             { blog && (
                 <article>
+                    <div className="breadcrumb">
+                        <Link to="/">back</Link>
+                        <Link to="/">Home</Link>
+                        <span></span>
+                    </div>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
